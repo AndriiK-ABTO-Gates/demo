4cf147913f426fa0c0f31adfeecde84410038d95
Andrii Kolodiichyk <ak8677@gates.com>
2025-04-27 08:56:25 +0300

    Project initialization
    Added WPF project and unit tests

---
+++ WpfDemoApp.Test/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿namespace WpfDemoApp.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task Test1()
+        {
+            await Task.Delay(1000);
+            Assert.Pass();
+        }
+
+        [Test]
+        public async Task Test2()
+        {
+            await Task.Delay(1500);
+            Assert.Pass();
+        }
+
+        [Test]
+        public async Task Test3()
+        {
+            await Task.Delay(2000);
+            Assert.Pass();
+        }
+
+        [Test]
+        public async Task Test4()
+        {
+            await Task.Delay(2500);
+            Assert.Pass();
+        }
+    }
+}
